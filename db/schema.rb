c99872d579b35bd791a69ccc54023ff4bc51af7d
angeluray <aeucampos@gmail.com>
2023-02-27 18:11:54 -0400

    Generate migrations for all versions (1st version)

---
+++ db/schema.rb
@@ -0,0 +1,143 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_220230) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "country_id", null: false
+    t.index ["country_id"], name: "index_cities_on_country_id"
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "fonte_id", null: false
+    t.bigint "job_type_id", null: false
+    t.index ["fonte_id"], name: "index_employees_on_fonte_id"
+    t.index ["job_type_id"], name: "index_employees_on_job_type_id"
+  end
+
+  create_table "fontes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "review_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "city_id", null: false
+    t.bigint "tag_id", null: false
+    t.bigint "market_category_id", null: false
+    t.index ["city_id"], name: "index_fontes_on_city_id"
+    t.index ["market_category_id"], name: "index_fontes_on_market_category_id"
+    t.index ["review_id"], name: "index_fontes_on_review_id"
+    t.index ["tag_id"], name: "index_fontes_on_tag_id"
+    t.index ["user_id"], name: "index_fontes_on_user_id"
+  end
+
+  create_table "job_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "market_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "fonte_id", null: false
+    t.bigint "product_tag_id", null: false
+    t.index ["fonte_id"], name: "index_products_on_fonte_id"
+    t.index ["product_tag_id"], name: "index_products_on_product_tag_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "description"
+    t.string "video"
+    t.string "image"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "role"
+    t.integer "posts_counter"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.bigint "city_id", null: false
+    t.index ["city_id"], name: "index_users_on_city_id"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "cities", "countries"
+  add_foreign_key "employees", "fontes"
+  add_foreign_key "employees", "job_types"
+  add_foreign_key "fontes", "cities"
+  add_foreign_key "fontes", "market_categories"
+  add_foreign_key "fontes", "reviews"
+  add_foreign_key "fontes", "tags"
+  add_foreign_key "fontes", "users"
+  add_foreign_key "products", "fontes"
+  add_foreign_key "products", "product_tags"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "users", "cities"
+end
