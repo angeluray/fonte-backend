2326ee0853f0313875e34e9074c2d1731f0f8b88
angeluray <aeucampos@gmail.com>
2023-03-14 12:18:19 -0400

    Update user model validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :reviews
+  has_many :fontes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,4 +10,5 @@ class User < ApplicationRecord
   validates :state, presence: true
   validates :city, presence: true
 
+  
 end
