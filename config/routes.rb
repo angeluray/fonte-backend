cb6dfb2bb72b778f46542f4c7ea8c58cce3ec85f
angeluray <aeucampos@gmail.com>
2023-03-14 12:41:17 -0400

    Update routes and schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,55 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :market_categories
-  resources :tags
-  resources :products
-  resources :product_tags
-  resources :employees
-  resources :job_types
-  resources :fontes
-  resources :reviews
-  resources :users
-  resources :cities
-  resources :countries
+
+  resources :market_categories do
+    resources :fontes
+  end
+
+  resources :tags do
+    resources :fontes
+  end
+
+  resources :product_tags do
+    resources :products
+  end
+
+
+  resources :job_types do
+    resources :employees
+  end
+  
+
+  resources :users do
+    collection do
+      get :fetch_country_states
+      get :cities
+    end
+    resources :reviews
+    resources :fontes do
+      resources :employees
+      resources :products
+    end
+  end
+
+  resources :reviews do
+    resources :fontes
+  end
+
+  resources :fontes do
+    resources :employees
+    resources :products
+  end
+
+  # resources :cities
+  # resources :countries
+
+  # resources :addresses do
+  #   collection do
+  #     get :states
+  #   end
+  # end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_220230) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_012828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "country"
+    t.string "state"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -106,6 +142,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_220230) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.date "born_on"
     t.integer "age"
     t.string "role"
     t.integer "posts_counter"
@@ -124,13 +161,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_220230) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.bigint "city_id", null: false
-    t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cities", "countries"
   add_foreign_key "employees", "fontes"
   add_foreign_key "employees", "job_types"
@@ -142,5 +179,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_220230) do
   add_foreign_key "products", "fontes"
   add_foreign_key "products", "product_tags"
   add_foreign_key "reviews", "users"
-  add_foreign_key "users", "cities"
 end
