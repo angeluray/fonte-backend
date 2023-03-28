d8cb284daa41106238f2fa7524c303a7ba68eee2
angeluray <aeucampos@gmail.com>
2023-03-27 21:54:41 -0400

    Update port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   
