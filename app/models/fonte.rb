1a18e381f521f988eee71526233fd6342eae1193
angeluray <aeucampos@gmail.com>
2023-03-27 21:44:15 -0400

    Update models and add basic validation

--- app/models/fonte.rb
+++ app/models/fonte.rb
@@ -1,2 +1,6 @@
 class Fonte < ApplicationRecord
+    belongs_to :user
+    has_many :reviews
+
+    has_one_attached :profile_image
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,28 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :fonte
+  has_one_attached :image
+  has_one_attached :video
+
+  validate :img_or_vid_attached
+  validate :img_or_vid_format
+
+  private
+
+  def img_or_vid_attached
+    unless image.attached? || video.attached?
+        errors.add(:base, "Please attach an image or a video")
+    end
+  end
+
+  def img_or_vid_format
+    if image.attached? && !image.content_type.in?(%w[image/png image/jpg image/jpeg])
+        errors.add(:image, "must be a PNG, JPG, or JPEG")
+    end
+
+    if video.attached? && !video.content_type.in?(%w[video/mp4 video/webm])
+        errors.add(:video, "must be an MP4 or WebM")
+    end
+  end
+
 end
